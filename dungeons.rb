2404b90b76768d3551443ef6477493ee8098a2d4
Kevin Wahome <wahome@talicraft.com>
2018-02-14 00:02:07 +0300

    Added basic navigation to Dungeon

--- dungeons.rb
+++ dungeons.rb
@@ -17,7 +17,7 @@ class Dungeon
     show_current_description
   end
 
-  def go
+  def go(direction)
     puts "You go #{direction.to_s}"
     @player.location = find_room_in_direction(direction)
     show_current_description
@@ -65,5 +65,13 @@ my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", { :west 
 my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", { :east => :largecave })
 
 my_dungeon.start(:largecave)
+
+pp my_dungeon
+
+my_dungeon.go(:west)
+
+pp my_dungeon
+
+my_dungeon.go(:east)
 
 pp my_dungeon
