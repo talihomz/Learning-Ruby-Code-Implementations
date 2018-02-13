901004dcde9eb6640109a3a6ca34aa9cde1f13d4
Kevin Wahome <wahome@talicraft.com>
2018-02-13 23:37:13 +0300

    Added Person class implementation with getters and setters

---
+++ person_with_setters_and_getters.rb
@@ -0,0 +1,22 @@
+class Person
+
+  #nane getter
+  def name
+    @name
+  end
+
+  #name getter
+  def name=(name)
+    @name = name
+  end
+
+  #age getter
+  def age
+    @age
+  end
+
+  #age setter
+  def age=(age)
+    @age = age
+  end
+end
